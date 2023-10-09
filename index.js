33e2675d3282464e92a4f37f76f2cbf4c4f2d8ef
Max <nocapulus@gmail.com>
2023-10-09 16:09:10 -0400

    Express Session

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+app.use(session({ secret: 'footsies' }));
+
+app.get('/viewcount', (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`You have viewed this page ${req.session.count} times`);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
